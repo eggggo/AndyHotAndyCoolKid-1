a4bc3934dd3c762e06f3b5f790957a1bdaf79e84
eggggo <52181959+eggggo@users.noreply.github.com>
2019-06-29 10:08:48 -0400

    Update join_channel.js
    ali alibaba

--- commands/random/join_channel.js
+++ commands/random/join_channel.js
@@ -55,7 +55,8 @@ class JoinChannelCommand extends commando.Command {
                         "https://www.youtube.com/watch?v=5NHt1k_9-J8",
                         "https://www.youtube.com/watch?v=mj-v6zCnEaw",
                         "https://www.youtube.com/watch?v=0BvfVM0sBfI",
-                        "https://www.youtube.com/watch?v=u3Jr9t_jFcE&list=LL72cjNtbukMkxI0-ohcwotQ&index=128"
+                        "https://www.youtube.com/watch?v=u3Jr9t_jFcE&list=LL72cjNtbukMkxI0-ohcwotQ&index=128",
+                        "https://www.youtube.com/watch?v=DuVJeJcMod0"
                     ]}
                 }
                 message.member.voiceChannel.join()
