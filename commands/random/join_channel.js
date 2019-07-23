9deba1e55d362b2b8fb02ee85f08f5d0ab241e7e
eggggo <52181959+eggggo@users.noreply.github.com>
2019-07-22 21:46:53 -0400

    Update join_channel.js

--- commands/random/join_channel.js
+++ commands/random/join_channel.js
@@ -81,6 +81,7 @@ class JoinChannelCommand extends commando.Command {
 "https://www.youtube.com/watch?v=OiR7JHrSHdA", /* K.K. Slider: K.K. Cruisin' */
 "https://www.youtube.com/watch?v=OFjqEexH0Tg", /* Mermaid Sisters Song */
 "https://youtu.be/atuFSv2bLa8", /* Manuel - Gas Gas Gas */
+"https://www.youtube.com/watch?v=BJ0xBCwkg3E&list=PLYst6aAB-EKfs_Gyr9HTuRnlcLws8qvWH", /*Running in the 90's*/
 "https://www.youtube.com/watch?v=bsOv7069Vsg"/*Interstellar Remix*/
                     ]}
                 }
