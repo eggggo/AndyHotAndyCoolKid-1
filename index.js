8fffcae193c3b4f68a2c0c92168d05ff48b350a4
eggggo <52181959+eggggo@users.noreply.github.com>
2019-07-23 22:05:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ client.on('message', message=>{
     message.channel.send('me :)')
   }
   else if (message.content.toLowerCase().substring(0,3).includes('is ') || 
-           message.content.toLowerCase().includes('does ') || 
+           message.content.toLowerCase().substring(0,5).includes('does ') || 
            message.content.toLowerCase().substring(0,7).includes('should ') ||
            message.content.toLowerCase().substring(0,4).includes('was ') || 
            message.content.toLowerCase().substring(0,5).includes('will ') || 
